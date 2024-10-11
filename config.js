4ee4ee01dce770e607b1cbd1bbc990bac4cc638d
rishi098456 <aryan.kk05@gmail.com>
2024-10-11 16:23:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XqIG1DoB#poNBljlCm_gwyn2YHG8JKb6lB1qpPNtMj-IoDxpjvCM",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
